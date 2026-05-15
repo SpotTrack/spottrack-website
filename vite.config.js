972f394f84f5a219f539fa654bf3a931bf2f16a4
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-05-15 01:59:27 -0500

    creating routes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/spottrack-website/',  // ← add this
   plugins: [vue()],
   server: {
     port: 4200,
