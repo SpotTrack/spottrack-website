b54d9be4649af142f14162ad47d4cbcb7c3c5940
jcus1510 <167936372+jcus1510@users.noreply.github.com>
2026-05-15 01:11:08 -0500

    feat:add-footer-section

---
+++ src/components/FooterSection/FooterSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer-section">
+    <div class="container">
+      <p>{{ $t('footer.copyright') }}</p>
+    </div>
+  </footer>
+</template>
+
+<script src="./FooterSection.js"></script>
+
+<style src="./FooterSection.css" scoped></style>
+
